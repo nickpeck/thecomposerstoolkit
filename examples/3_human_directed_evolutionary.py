4dd9dc3af5f29f60137cc3cad4609767c871b6e3
Nick Peck <sysadmin@take1.tv>
2021-03-13 11:37:48 -0800

    adds example 3_human_directed_evolutionary.py

---
+++ examples/3_human_directed_evolutionary.py
@@ -0,0 +1,64 @@
+import pprint
+
+import fluidsynth
+
+from composerstoolkit import (CTEvent, CTSequence, CTGenerator,  cantus,
+CTTransformer, random_walk, random_walk_backtracking, Container, chain,
+random_walk_backtracking_w_heuristics, Evolutionary, Extinction, steady_pulse,
+transpose, constraint_in_set, heuristic_trend_upwards, heuristic_sine_shape, map_to_pulses,
+transpose, retrograde, invert, rotate, explode_intervals, scales)
+
+# intialise our synth
+synth = fluidsynth.Synth()
+synth.start()
+sfid = synth.sfload("Nice-Steinway-v3.8.sf2")
+synth.program_select(0, sfid, 0, 0)
+
+starting_transformations = [
+    (transpose(1), 0.5),
+    (retrograde(), 0.5),
+    (invert(), 0.5),
+    (rotate(), 0.5),
+    (explode_intervals(2), 0.5)]
+    
+def playback(seq):
+    pulse_pattern = steady_pulse(0.2, 100)
+    seq = seq |chain| map_to_pulses(pulse_pattern)
+    container = Container(bpm=160)
+    container.add_sequence(0, seq)
+    container.playback(synth)
+    
+def present_results(result, transformations):
+    print("="*20)
+    print("The final resulting sequence is :")
+    pprint.pprint(result.events, indent=4)
+    print("The resulting pool of transformations is :")
+    pprint.pprint(transformations, indent=4)
+    playback(result)
+    
+def get_feedback(seq):
+    print("-"*20)
+    pprint.pprint(seq.events, indent=4)
+    playback(seq)
+    response = input("Press Y to keep, any key to reject: ")
+    return response.strip().upper() == "Y"
+    
+evo = Evolutionary(
+    transformations=starting_transformations,
+    fitness_func=get_feedback)
+
+result,transformations = evo(cantus([60, 66, 67]))
+present_results(result,transformations)
+
+while True:
+    res = input("Enter y to run, unguided for 10 more generations, any other key to end")
+
+    if res.strip().upper() == "Y":
+        evo = Evolutionary(
+            transformations=transformations,
+            fitness_func=lambda x : True) # unguided
+
+        result,transformations = evo(result, 10)
+        present_results(result,transformations)
+    else:
+        break
